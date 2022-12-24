5f7b22318eb6b491401154f46984e70ba49681d7
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-12-24 11:46:13 +0200

    cast

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -1,3 +1,58 @@
+import { useState, useEffect } from 'react';
+import {
+  useParams,
+  useLocation,
+} from 'react-router-dom';
+
+  const KEY = '5b1449ced393d87bc0a1ea0f9fb4bc3e';
+  const BASEURL_ACTOR = 'https://api.themoviedb.org/3/movie/';
+
+
 export const Cast = () => {
-  return <div>Cast 1</div>;
+
+
+  const { movieId } = useParams();
+  const [actors, setActors] = useState([]);
+  const location = useLocation();
+  const goBack = location.state?.from ?? '/';
+
+  useEffect(() => {
+    async function fetchMoviesDetails() {
+      try {
+        await fetch(`${BASEURL_ACTOR}${movieId}/credits?api_key=${KEY}`)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(response.status);
+            }
+            return response.json();
+          })
+          .then(data => {
+            setActors(data.cast);
+                });
+      } catch {
+        window.alert('щось пішло не так');
+      }
+    }
+    fetchMoviesDetails();
+  }, [movieId]);
+
+  
+  return (
+    <div>
+      {actors && (
+        <ul>
+          {actors.map(({ id, name, profile_path, character }) => (
+            <li key={id}>
+              <img
+                src={`https://www.themoviedb.org/t/p/w200${profile_path}`}
+                alt={name}
+              />
+              <div>{name}</div>
+              <div>Character: {character}</div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,7 +1,48 @@
+import { useState, useEffect } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+
+const KEY = '5b1449ced393d87bc0a1ea0f9fb4bc3e';
+const BASEURL_ACTOR = 'https://api.themoviedb.org/3/movie/';
+
 export const Reviews = () => {
-  return <div>Reviews 1</div>;
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+  const location = useLocation();
+  console.log('location', location.pathname);
+  const goBack = location.state?.from ?? '/';
+
+  useEffect(() => {
+    async function fetchMoviesDetails() {
+      try {
+        await fetch(`${BASEURL_ACTOR}${movieId}/reviews?api_key=${KEY}`)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(response.status);
+            }
+            return response.json();
+          })
+          .then(data => {
+            setReviews(data.results);
+            console.log('data Reviews', data.results.length);
+          });
+      } catch {
+        window.alert('щось пішло не так');
+      }
+    }
+    fetchMoviesDetails();
+  }, [movieId]);
+  return (
+    <div>
+      {reviews.length > 0 ? (
+        <ul>
+          {reviews.map(({ author, content }) => (
+            <li key={author}>
+              <div>{author}</div>
+              <div> {content}</div>
+            </li>
+          ))}
+        </ul>
+      ):<div>not info</div>}
+    </div>
+  );
 };
-
-
-
-
